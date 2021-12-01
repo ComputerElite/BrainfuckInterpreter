0beef13a8cb99f63fb5ea6e0c3e9045a3b98fd89
ComputerElite <>
2021-12-01 21:11:14 +0100

    fix loops  a g a i n

--- Program.cs
+++ Program.cs
@@ -50,11 +50,22 @@ class Program
         Console.WriteLine("");
         while (programPosition < brainfuck.Length)
         {
-            if (lastLoopOpen.Count >= 1 && lastLoopOpen[0] == -1)
+            if (args.Length >= 2 && args[1] == "debug" || input == "DEBUG")
+            {
+                Console.SetCursorPosition(0, 10);
+                Console.Write("Open loops: " + lastLoopOpen.Count + ", loop 0: " + (lastLoopOpen.Count >= 1 ? lastLoopOpen[0] : -2) + "\nPosition: " + programPosition);
+                Console.SetCursorPosition(0, 1);
+                Console.WriteLine(new String(' ', programPosition) + "^" + new String(' ', brainfuck.Length));
+                Display();
+                Console.ReadKey();
+            }
+
+            if (lastLoopOpen.Count >= 1 && lastLoopOpen[0] == -1 && brainfuck[programPosition] != ']' && brainfuck[programPosition] != '[')
             {
                 programPosition++;
                 continue;
             }
+
             switch (brainfuck[programPosition])
             {
                 case '<': // Decrease pointer
@@ -75,42 +86,26 @@ class Program
                     Console.Write((char)memory[pointer]);
                     break;
                 case '[': // Open loop
-                    lastLoopOpen.Insert(0, programPosition + 1);
-                    if (memory[pointer] == 0) lastLoopOpen[0] = -1;
+                    lastLoopOpen.Insert(0, memory[pointer] == 0 || lastLoopOpen.Count >= 1 && lastLoopOpen[0] == -1 ? -1 : programPosition);
                     break;
                 case ']': // Close loop
-                    if (memory[pointer] != 0)
+                    if (memory[pointer] == 0)
                     {
-                        programPosition = lastLoopOpen[0];
-                        //Console.SetCursorPosition(0, 0);
-                        //Console.WriteLine("Hopping to " + lastLoopOpen[0]);
-                        continue;
+                        lastLoopOpen.RemoveAt(0);
+                        break;
                     }
+                    programPosition = lastLoopOpen[0];
                     lastLoopOpen.RemoveAt(0);
-                    break;
+                    continue;
                 case ',': // Set the memory to the inputted key
                     memory[pointer] = (byte)Console.ReadKey(true).KeyChar;
                     break;
-                default:
-                    // Don't do anything in case there are any comments or line breaks
-                    break;
 
             }
-            if(args.Length >= 2 && args[1] == "debug" || input == "DEBUG")
-            {
-                Console.SetCursorPosition(0, 1);
-                Console.WriteLine(new String(' ', programPosition) + "^" + new String(' ', brainfuck.Length));
-                Display();
-                Thread.Sleep(500);
-            }
-            
-            
-            
-            
             programPosition++;
         }
         Console.WriteLine("");
         Console.WriteLine("");
-        Console.WriteLine("");
+        //Console.WriteLine(programPosition);
     }
 }
